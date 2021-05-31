8e039cfdc1bf787cf042b11aa495b8eef89a4078
hugovallada <valladahugo@gmail.com>
2021-05-31 10:25:42 -0300

    Configura o application.yml e o build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,17 @@ plugins {
     id("com.github.johnrengelman.shadow") version "7.0.0"
     id("io.micronaut.application") version "1.5.0"
     id("org.jetbrains.kotlin.plugin.allopen") version "1.4.32"
+    id("org.jetbrains.kotlin.plugin.jpa") version "1.4.32"
+    id("org.jetbrains.kotlin.plugin.noarg") version "1.5.10"
     id("com.google.protobuf") version "0.8.15"
+}
+
+allOpen{
+    annotation("javax.inject.Singleton")
+}
+
+noArg{
+    annotation("javax.persistence.Entity")
 }
 
 version = "0.1"
@@ -25,18 +35,30 @@ micronaut {
 }
 
 dependencies {
+    kapt("io.micronaut.data:micronaut-data-processor")
+    implementation("io.micronaut.data:micronaut-data-hibernate-jpa")
+    implementation("io.micronaut.sql:micronaut-jdbc-hikari")
+    runtimeOnly("org.postgresql:postgresql")
     implementation("io.micronaut:micronaut-runtime")
     implementation("io.micronaut.grpc:micronaut-grpc-runtime")
     implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
     implementation("javax.annotation:javax.annotation-api")
+    implementation("io.micronaut:micronaut-http-client")
     implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
     runtimeOnly("ch.qos.logback:logback-classic")
     implementation("io.micronaut:micronaut-validation")
+    implementation("io.micronaut.beanvalidation:micronaut-hibernate-validator")
+
 
     runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
-
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:postgresql")
+    testImplementation("org.testcontainers:testcontainers")
     testImplementation("io.micronaut:micronaut-http-client")
+    testImplementation("com.h2database:h2")
+    testImplementation("org.mockito:mockito-core:3.8.0")
+
 
 }
 
@@ -59,6 +81,7 @@ tasks {
             jvmTarget = "11"
         }
     }
+
 
 
 }
